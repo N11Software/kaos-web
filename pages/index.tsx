7c86f1b90790f8ea5ff85eebe8c5be796abbde87
Ariston Lorenzo <4tl0renz0@gmail.com>
2022-03-12 21:54:52 -0500

    pages: Add header
    Signed-off-by: Ariston Lorenzo <4tl0renz0@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -10,9 +11,23 @@ const Home: NextPage = () => {
         <title>Kaos</title>
         <meta name="description" content="Kaos, your place to create, discuss, and play your game." />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.main}>
+        <header className={styles.header}>
+          <h1 className={styles.headerLogo}>KAOS</h1>
+          <div className={styles.headerLinks}>
+            <a className={styles.headerLink} href="store/">STORE</a>
+            <a className={styles.headerLink} href="community/">COMMUNITY</a>
+            <a className={styles.headerLink} href="about/">ABOUT</a>
+          </div>
+          <div className={styles.headerLogin}>
+            <a className={styles.headerLoginLink} href="login/">LOGIN</a>
+          </div>
+        </header>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -65,7 +80,7 @@ const Home: NextPage = () => {
           </span>
         </a>
       </footer>
-    </div>
+    </div >
   )
 }
 
