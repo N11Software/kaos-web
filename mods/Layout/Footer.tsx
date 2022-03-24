b54b72351792e183d1b52b99fd3a719f7eaf774f
Ariston Lorenzo <aristonl@n11software.com>
2022-03-24 12:06:51 -0400

    footer: Make copyright text caps and smaller
    Signed-off-by: Ariston Lorenzo <aristonl@n11software.com>

--- mods/Layout/Footer.tsx
+++ mods/Layout/Footer.tsx
@@ -5,7 +5,7 @@ let Footer = () => {
     <a href="https://n11software.com">
       <Image src="/images/N11-Logo-Alternate-Transparent.png" width={75} height={50} />
     </a>
-    <p className="text-white text-lg text-center">Copyright © N11 Software. All rights reserved. All trademarks are property of their respective owners in the US and other countries.</p>
+    <p className="text-white text-lg text-center" id="copyrightFooter">COPYRIGHT © 2022 N11 SOFTWARE. ALL RIGHTS RESERVED.</p>
   </footer>
 }
 
