610801a02678a29fc83a8cd84718daad9ac8000c
Ariston Lorenzo <aristonl@n11software.com>
2022-04-10 20:58:23 -0400

    footer: Add the N11 logo and make it slightly smaller
    Signed-off-by: Ariston Lorenzo <aristonl@n11software.com>

--- mods/Layout/Footer.tsx
+++ mods/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 let Footer = () => {
   return <footer className="flex py-8 justify-center items-center bg-black space-x-8">
     <a href="https://n11software.com">
-      <Image src="/images/N11-Logo-Alternate-Transparent.png" width={75} height={50} />
+      <Image src="/images/N11-Logo-Alternate-Transparent.png" width={47} height={31} />
     </a>
     <p className="text-white text-lg text-center" id="copyrightFooter">COPYRIGHT © 2022 N11 SOFTWARE. ALL RIGHTS RESERVED.</p>
   </footer>
